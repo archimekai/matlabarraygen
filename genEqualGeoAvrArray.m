28d1705b507dec6c187ec06a139478c5314c092a
archimekai <wenkai123111@126.com>
2015-04-30 19:19:43 +0800

    generate an array that has a specified geometric average

---
+++ genEqualGeoAvrArray.m
@@ -0,0 +1,87 @@
+function [ array ] = genEqualGeoAvrArray( geoavr, n, varargin )
+%GENEQUALGEOAVRARRAY 
+%   genEqualGeoAvrArray( geoavr, n [, lastnumber, minval, maxval  ] )
+%   parameters:
+%   geoavr: the Geometric means of the array to be generated
+%   lastnumber:  the last number of the array
+%   n: the length of the array
+%   minval: the minimum value allowed for each element in the array
+%   maxval: the minimum value allowed for each element in the array, this
+%       might cause long computing time, use this parameter with caution!
+%   by wenkai@Department of Psychology, Peking Universtiy. bliewhale@163.com. 20150425
+
+switch nargin
+    case 2
+            randpart = n*log(geoavr);
+            randratio = rand(1,n);
+            randratio = randratio / sum(randratio);  % 标准化
+            randratio = randpart * randratio;
+            array = exp(randratio);
+    case 3
+        lastnumber = varargin{1};
+        randpart = n*log(geoavr) - log(lastnumber);
+        randratio = rand(1,n-1);
+        randratio = randratio / sum(randratio);  % 标准化
+        randratio = randpart * randratio;
+        array = exp(randratio);
+        array(n) = lastnumber;
+    case 4
+        lastnumber = varargin{1};
+        minval = varargin{2}; 
+        if minval > 0
+            if log(minval)*(n-1) + log(lastnumber)>n*log(geoavr)
+                warning('min is too big!\n');
+                array = 0;
+                return;
+            end
+
+            randpart = n*log(geoavr) - log(lastnumber) - (n - 1) * log(minval);
+            randratio = rand(1,n-1);
+            randratio = randratio / sum(randratio);  % 标准化
+            randratio = randpart * randratio;
+            array = exp(randratio + log(minval));
+            array(n) = lastnumber;
+        else
+            warning('minval must be a positive real number!\n');
+            randpart = n*log(geoavr) - log(lastnumber);
+            randratio = rand(1,n-1);
+            randratio = randratio / sum(randratio);  % 标准化
+            randratio = randpart * randratio;
+            array = exp(randratio);
+            array(n) = lastnumber;
+        end
+    case 5
+        lastnumber = varargin{1};
+        minval = varargin{2}; 
+        maxval = varargin{3};
+        array = Inf([1,n]);
+        if maxval <= minval
+            warning('maxval must be larger than minval!\nWill ignore maxval!\n');
+            maxval = Inf;
+        end
+        while (max(array(1:n - 1)) > maxval)
+            if minval > 0
+                if log(minval)*(n-1) + log(lastnumber)>n*log(geoavr)
+                    warning('min is too big!\n');
+                    array = 0;
+                    return;
+                end
+
+                randpart = n*log(geoavr) - log(lastnumber) - (n - 1) * log(minval);
+                randratio = rand(1,n-1);
+                randratio = randratio / sum(randratio);  % 标准化
+                randratio = randpart * randratio;
+                array = exp(randratio + log(minval));
+                array(n) = lastnumber;
+            else
+                warning('minval must be a positive real number!\n');
+                randpart = n*log(geoavr) - log(lastnumber);
+                randratio = rand(1,n-1);
+                randratio = randratio / sum(randratio);  % 标准化
+                randratio = randpart * randratio;
+                array = exp(randratio);
+                array(n) = lastnumber;
+            end
+        end
+end
+
